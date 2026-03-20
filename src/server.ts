9d7052faa7951986eaa22d31efa61efb80f3119a
omkarpatole1799 <omkarpatole1799@gmail.com>
2026-03-20 23:46:05 +0530

    feat: added prom-client for metrics collection

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,25 @@
-// src/server.ts
 import * as dotenv from "dotenv";
 dotenv.config({
   path: "./.env",
 });
 
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import errorHandler from "./middlewares/errorHandler";
 import indexRouter from "./routes/index.routes";
 import connectMongo from "./utils/db.connect";
+import addPromClient from "./utils/add-prom-client";
 dotenv.config({
   path: "./.env",
 });
 
 const app: Application = express();
 const port: number | string = process.env["PORT"] || 3000;
+
+import client from "prom-client";
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+collectDefaultMetrics({ register: client.register });
 
 (async () => {
   await connectMongo();
@@ -26,6 +32,12 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api", indexRouter);
+
+app.get("/metrics", async (rq: Request, rs: Response, next: NextFunction) => {
+  rs.setHeader("Content-Type", client.register.contentType);
+  const metrics = await client.register.metrics();
+  rs.send(metrics);
+});
 
 app.use(errorHandler);
 
