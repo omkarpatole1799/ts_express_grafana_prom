88649f66112684831b6c231afe558c67f6f6e345
omkarpatole1799 <omkarpatole1799@gmail.com>
2026-03-21 10:16:23 +0530

    chore: nodemon added

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,19 +1,29 @@
 import { log } from "node:console";
-import ApiResponse from "../utils/api-response";
-import tryCatch from "../utils/tryCatch";
 import ApiError from "../utils/api-error";
+import ApiResponse from "../utils/api-response";
+import { getDb } from "../utils/db.connect";
+import tryCatch from "../utils/tryCatch";
 
 const register = tryCatch(async (req, res, next) => {
   const { name, age } = req.body;
+  log(name, age);
+
   if (name?.trim() === "" || !name) {
     throw new ApiError(400, "Name required");
   }
-  if (!age || !isNaN(age)) {
+  if (!age || isNaN(age)) {
     throw new ApiError(400, "Invalid age");
   }
-  return new ApiResponse("Hi", {
+
+  const newUser = await getDb().collection("users").insertOne({
     name,
     age,
+  });
+
+  console.log(newUser, "-newUser");
+
+  return new ApiResponse("Successfully created new user", {
+    userId: newUser.insertedId,
   });
 });
 
